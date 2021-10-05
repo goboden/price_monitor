ceefa266b8c878cc3864e8c471d2fd8d0bcee7bb
Alex Gatsenko <jocker.fun@gmail.com>
2021-10-05 20:56:13 +0400

    add create_db and models

--- create_db.py
+++ create_db.py
@@ -1,8 +1,18 @@
 import os
+from sqlalchemy.orm import sessionmaker
+from models import db, User, Goods, Price
 from sqlalchemy import create_engine
-from models import User, Price, Goods, db
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-engine = create_engine('sqlite:///' + os.path.join(basedir, 'price_monitor.db'), echo=True)
-db.metadata.create_all(engine)
+def main():
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    engine = create_engine('sqlite:///' + os.path.join(basedir, 'price_monitor.db'), echo=True)
+
+    db.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -1,46 +1,51 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
 
-db = SQLAlchemy()
+db = declarative_base()
 
 
-class Price(db.Model):
+class Price(db):
     """
     period, goods_id (fk), price. id (pk)
     """
-    period = db.Column(db.DateTime, nullable=False)
-    goods_id = db.Column(db.Integer, db.ForeignKey('goods.id'), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'price'
+
+    id = Column(Integer, primary_key=True)
+    period = Column(DateTime, nullable=False)
+    goods_id = Column(Integer, ForeignKey('goods.id'), nullable=False)
+    price = Column(Float, nullable=False)
+
 
     def __repr__(self):
         return f'<{self.period} {self.price}>'
 
 
-class Goods(db.Model):
+class Goods(db):
     """
     Поля id (pk), user_id (fk), url, check_period.
     """
+    __tablename__ = 'goods'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
-    check_period = db.Column(db.DateTime, nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    url = Column(String, unique=True, nullable=False)
+    check_period = Column(DateTime, nullable=False)
 
     def __repr__(self):
         return f'<{self.user_id} {self.url} {self.check_period}>'
 
 
-class User(db.Model, UserMixin):
+class User(db):
     """
     id (pk), telegram_id, username, password
     """
+    __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    telegram_id = db.Column(db.String(64), index=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password = db.Column(db.String(128))
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(String(64), index=True)
+    username = Column(String(64), index=True, unique=True)
+    password = Column(String(128))
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
