5c07ecc3db3c9326774b68150c95da1427fbc347
AlexSG8 <46405594+AlexSG8@users.noreply.github.com>
2021-10-05 21:00:53 +0400

    Delete create_db.py

--- create_db.py
+++
@@ -1,8 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from models import User, Price, Goods, db
-
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-engine = create_engine('sqlite:///' + os.path.join(basedir, 'price_monitor.db'), echo=True)
-db.metadata.create_all(engine)
