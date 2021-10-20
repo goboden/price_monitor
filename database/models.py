65fe958c00ae7975c8e839cdae652da7948fd3eb
goboden <goboden@yandex.ru>
2021-10-20 12:52:11 +0300

    New relationships and test

--- database/models.py
+++ database/models.py
@@ -1,10 +1,15 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy import Table, UniqueConstraint
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin
 
 
 Base = declarative_base()
+
+user_goods_table = Table('user_goods', Base.metadata,
+                         Column('user_id', ForeignKey('users.id')),
+                         Column('goods_id', ForeignKey('goods.id')))
 
 
 class User(Base, UserMixin):
@@ -14,6 +19,9 @@ class User(Base, UserMixin):
     name = Column(String(64))
     password = Column(String(128), unique=True)
     telegram = relationship('Telegram', back_populates='user', uselist=False)
+    goods = relationship('Goods',
+                         secondary=user_goods_table,
+                         back_populates='users')
 
 
 class Telegram(Base):
@@ -22,27 +30,28 @@ class Telegram(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
     chat_id = Column(Integer, nullable=False)
-    
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     user = relationship('User', back_populates='telegram')
-
-
-class Price(Base):
-    __tablename__ = 'prices'
-
-    id = Column(Integer, primary_key=True)
-    goods_id = Column(Integer, ForeignKey('goods.id'), nullable=False)
-    check_date = Column(DateTime, nullable=False)
-    price = Column(Float, nullable=False)
 
 
 class Goods(Base):
     __tablename__ = 'goods'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     url = Column(String, unique=True, nullable=False)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    image = Column(String, nullable=False)
-    check_date = Column(DateTime, nullable=False)
+    name = Column(String)
+    description = Column(String)
+    image = Column(String)
+    users = relationship('User',
+                         secondary=user_goods_table,
+                         back_populates='goods')
+    prices = relationship('Price')
+
+
+class Price(Base):
+    __tablename__ = 'prices'
+    id = Column(Integer, primary_key=True)
+    goods_id = Column(Integer, ForeignKey('goods.id'), nullable=False)
+    date = Column(DateTime, nullable=False)
+    price = Column(Float, nullable=False)
+    __table_args__ = (UniqueConstraint('goods_id', 'date'), )
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,8 +1,9 @@
 import os
 from database import engine, session
-from database.models import Base, User
+from database.models import Base, User, Telegram, Goods, Price
 from database import users
 from database.exceptions import UserExistsError
+from datetime import datetime, timedelta
 import pytest
 
 
@@ -14,11 +15,45 @@ def prepare(request):
     yield
 
     session.close()
-    os.remove(db_name)
+    #os.remove(db_name)
 
 
 @pytest.mark.database
-def test_create_user1(prepare):
+def test_create_objects(prepare):
+    print(' ')
+    telegram = Telegram(id=1234567890, name='@user', chat_id=987654321)
+    user = User(name='SampleUser')
+    telegram.user = user
+    session.add(user)
+
+    another_user = User(name='AnotherUser')
+    session.add(another_user)
+
+    goods = Goods(url='http://goods.ru/g1', name='G1')
+    goods.users.append(user)
+    goods.users.append(another_user)
+    session.add(goods)
+
+    price1 = Price(date=datetime.now(), price=100)
+    goods.prices.append(price1)
+    price2 = Price(date=(datetime.now() + timedelta(minutes=1)), price=200)
+    goods.prices.append(price2)
+    session.add_all((price1, price2))
+
+    session.commit()
+
+    user = session.query(Telegram).filter_by(id=1234567890).first().user
+    print(f'user={user.name}')
+    for goods_item in user.goods:
+        print(f' ... goods={goods_item.name} ({goods_item.url})')
+        for price_record in goods.prices:
+            print(f' ... ... price={price_record.price}')
+        for goods_user in goods.users:
+            print(f' ... ... user={goods_user.name}')
+
+
+@pytest.mark.database
+def qtest_create_user1(prepare):
     user_name = '@test_user'
     user_telegram_id = 123456789
     user_chat_id = 987654321
@@ -27,7 +62,7 @@ def test_create_user1(prepare):
 
 
 @pytest.mark.database
-def test_create_user1_twice(prepare):
+def qtest_create_user1_twice(prepare):
     user_name = '@test_user'
     user_telegram_id = 123456789
     user_chat_id = 987654321
@@ -37,17 +72,17 @@ def test_create_user1_twice(prepare):
 
 
 @pytest.mark.database
-def test_create_user2(prepare):
+def qtest_create_user2(prepare):
     users.add('@test_user2', 123456700, 1000654321)
 
 
 @pytest.mark.database
-def test_create_user3(prepare):
+def qtest_create_user3(prepare):
     users.add('@test_user3', 123456701, 1000654321)
 
 
 @pytest.mark.database
-def test_get_user_by_telegram_id(prepare):
+def qtest_get_user_by_telegram_id(prepare):
     user_telegram_id = 123456700
     user = users.get_by_teleram_id(user_telegram_id)
     assert user.telegram.chat_id == 1000654321
@@ -55,7 +90,7 @@ def test_get_user_by_telegram_id(prepare):
 
 
 @pytest.mark.database
-def test_password(prepare):
+def qtest_password(prepare):
     telegram_id = 123456700
     password = users.new_password_from_telegram(telegram_id)
     user = users.get_by_password(password)
