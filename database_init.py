88703203b7d74d00cd6c1a12ce08a8b3105c52d5
goboden <goboden@yandex.ru>
2021-11-06 11:48:30 +0300

    Minor changes in database init

--- database_init.py
+++ database_init.py
@@ -5,7 +5,6 @@ from database.models import db, User, Telegram, Goods, Price
 from database import generate_hash
 from datetime import datetime, timedelta
 
-
 engine = create_engine(DB_URI)
 session = scoped_session(sessionmaker(bind=engine))
 
@@ -16,10 +15,10 @@ def create_db():
 
 def add_users():
     users = (
-        (1, 'Test User 1', 1234567890, 1234567, '1'),
-        (2, 'Test User 2', 1234567891, 1234567, '2'),
-        (3, 'Test User 3', 1234567892, 1234567, '3'),
-        (4, 'Test User 4', 1234567893, 1234567, '4'),
+        (1, 'Test User 1', 1234567890, 11032511, '1'),
+        (2, 'Test User 2', 1234567891, 11032511, '2'),
+        (3, 'Test User 3', 1234567892, 11032511, '3'),
+        (4, 'Test User 4', 1234567893, 11032511, '4'),
     )
     for user in users:
         add_user(*user)
@@ -46,14 +45,15 @@ def add_goods_item(id, user_id, title, url, image, description, price):
     goods_item = session.query(Goods).filter_by(url=url).first()
     if not goods_item:
         goods_item = Goods(id=id,
-                        title=title,
-                        url=url,
-                        image=image,
-                        description=description)
+                           title=title,
+                           url=url,
+                           image=image,
+                           description=description)
     goods_item.users.append(user)
     session.add(goods_item)
     for minutes in range(1, 10):
-        price_item = Price(check_date=datetime.now() - timedelta(minutes=minutes),
+        price_item = Price(check_date=datetime.now() -
+                           timedelta(minutes=minutes),
                            price=price)
         price_item.goods = goods_item
         session.add(price_item)
@@ -61,56 +61,35 @@ def add_goods_item(id, user_id, title, url, image, description, price):
 
 
 goods = (
-        (1,
-         1,
-         'Warhammer 40,000: Recruit Edition',
-         'https://hobbygames.ru/warhammer-40000-recruit-edition',
-         'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Starter/WH40k_Recruit_Edition_Starter-1024x1024-wm.jpg',
-         'Описание',
-         3176.0),
-        (2,
-         1,
-         'Start Collecting! Chaos Space Marines',
-         'https://hobbygames.ru/start-collecting-chaos-space-marines-2019',
-         'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Chaos_Space_Marines/Start-Collecting-Chaos-Space-Marines-1024x1024-wm.jpg',
-         'Описание',
-         5859.0),
-        (3,
-         1,
-         'Combat Patrol: Space Marines',
-         'https://hobbygames.ru/combat-patrol-space-marines',
-         'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Space_Marines/HG/WH40k_SM_Combat_Patrol-1024x1024-wm.jpg',
-         'Описание',
-         8301.0),
-        (4,
-         1,
-         'Necron Warriors and Paint Set',
-         'https://hobbygames.ru/necron-warriors-and-paint-set',
-         'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Paints/Set_and_Instrument/WH40k_Necron_Warriors_Paints_Set-1024x1024-wm.jpg',
-         'Описание',
-         2200.0),
-        (5,
-         1,
-         'Combat Patrol: Dark Angels',
-         'https://hobbygames.ru/combat-patrol-dark-angels',
-         'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Temp/300121/Combat_Patrol_Dark_Angels-1024x1024-wm.jpg',
-         'Описание',
-         8301.0),
-        (6,
-         2,
-         'Warhammer 40,000: Recruit Edition',
-         'https://hobbygames.ru/warhammer-40000-recruit-edition',
-         'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Starter/WH40k_Recruit_Edition_Starter-1024x1024-wm.jpg',
-         'Описание',
-         3176.0),
-        (7,
-         3,
-         'Warhammer 40,000: Recruit Edition',
-         'https://hobbygames.ru/warhammer-40000-recruit-edition',
-         'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Starter/WH40k_Recruit_Edition_Starter-1024x1024-wm.jpg',
-         'Описание',
-         3176.0),
-    )
+    (1, 1, 'Warhammer 40,000: Recruit Edition',
+     'https://hobbygames.ru/warhammer-40000-recruit-edition',
+     'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Starter/WH40k_Recruit_Edition_Starter-1024x1024-wm.jpg',
+     'Описание', 3176.0),
+    (2, 1, 'Start Collecting! Chaos Space Marines',
+     'https://hobbygames.ru/start-collecting-chaos-space-marines-2019',
+     'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Chaos_Space_Marines/Start-Collecting-Chaos-Space-Marines-1024x1024-wm.jpg',
+     'Описание', 5859.0),
+    (3, 1, 'Combat Patrol: Space Marines',
+     'https://hobbygames.ru/combat-patrol-space-marines',
+     'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Space_Marines/HG/WH40k_SM_Combat_Patrol-1024x1024-wm.jpg',
+     'Описание', 8301.0),
+    (4, 1, 'Necron Warriors and Paint Set',
+     'https://hobbygames.ru/necron-warriors-and-paint-set',
+     'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Paints/Set_and_Instrument/WH40k_Necron_Warriors_Paints_Set-1024x1024-wm.jpg',
+     'Описание', 2200.0),
+    (5, 1, 'Combat Patrol: Dark Angels',
+     'https://hobbygames.ru/combat-patrol-dark-angels',
+     'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Temp/300121/Combat_Patrol_Dark_Angels-1024x1024-wm.jpg',
+     'Описание', 8301.0),
+    (6, 2, 'Warhammer 40,000: Recruit Edition',
+     'https://hobbygames.ru/warhammer-40000-recruit-edition',
+     'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Starter/WH40k_Recruit_Edition_Starter-1024x1024-wm.jpg',
+     'Описание', 3176.0),
+    (7, 3, 'Warhammer 40,000: Recruit Edition',
+     'https://hobbygames.ru/warhammer-40000-recruit-edition',
+     'https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Games_Workshop_New/Warhammer_40k/Starter/WH40k_Recruit_Edition_Starter-1024x1024-wm.jpg',
+     'Описание', 3176.0),
+)
 
 if __name__ == "__main__":
     print('Init start')
